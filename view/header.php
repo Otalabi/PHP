f7f7e20802b157ef18255281da6cd3d6c27e0ea3
Otalabi <talabisteve@gmail.com>
2015-08-06 19:17:53 -0400

    PHP

---
+++ view/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Talabi Lab 9</title>
+        <link rel="stylesheet" type="text/css" href="/talab4/lab09/main.css" />
+    </head>
+    <body>
+    <div id="page">
+        <div id="header">
+            <h1>SportsPro Technical Support</h1>
+            <p>Sports management software for the sports enthusiast</p>
+            <ul class="nav"><li><a href="/talab4/lab09/">Home</a></li></ul>
+        </div>
+       
